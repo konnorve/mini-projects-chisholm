cf24ca5356c9dc5af343cd40661af26e7987d681
Konnor von Emster <konnorvonemster@gmail.com>
2021-10-19 10:50:35 -0400

    pre-multithreading analysis, functional

--- 2021_trained_diel_LFC_motif_search/parental_control_LFC_clustering.py
+++ 2021_trained_diel_LFC_motif_search/parental_control_LFC_clustering.py
@@ -6,29 +6,38 @@ from matplotlib.patches import Circle
 import matplotlib.colors
 from pathlib import Path
 from Bio import SeqIO, SeqUtils
+from Bio.SeqRecord import SeqRecord
 import gffpandas.gffpandas as gffpd
 import seaborn as sns
+import os, sys
 
 from sklearn.cluster import KMeans, AgglomerativeClustering, SpectralClustering
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.decomposition import PCA
 
+TRANSLATION_TABLE = 11
+
 def load_data(proj_dir):
 
-    reference_df_path = proj_dir / 'Diel_LFC_results' / 'DGE_trained_reference_table.tsv'
+    reference_df_path = proj_dir / 'Diel_LFCs' / 'DGE_trained_reference_table.tsv'
     reference_df = pd.read_csv(reference_df_path, sep='\t', index_col='Gene ID')
 
-    results_df_path = proj_dir / 'Diel_LFC_results' / 'DGE_trained_results_table.tsv'
+    results_df_path = proj_dir / 'Diel_LFCs' / 'DGE_trained_results_table.tsv'
     results_df = pd.read_csv(results_df_path, sep='\t', index_col='Gene ID')
 
-    # ref_fasta = proj_dir / 'NATL2A_genome_references' / 'onlyNATL2A.fna'
-    # ref_genome_record = SeqIO.read(ref_fasta, 'fasta')
-    # ref_genome_seq = ref_genome_record.seq
-    # gff_file = proj_dir / 'NATL2A_genome_references' / 'onlyNATL2A.gff'
-    # annotation = gffpd.read_gff3(gff_file)
-    # attributes_df = annotation.attributes_to_columns()
+    ref_fasta = proj_dir / 'NATL2A_genome_references' / 'onlyNATL2A.fna'
+    ref_genome_record = SeqIO.read(ref_fasta, 'fasta')
+    ref_genome_seq = ref_genome_record.seq
 
-    return reference_df, results_df
+    gff_file = proj_dir / 'NATL2A_genome_references' / 'onlyNATL2A_w_sRNA.gff'
+    annotation = gffpd.read_gff3(gff_file)
+    attributes_df = annotation.attributes_to_columns()
+    attributes_df['Gene ID'] = attributes_df['ID']
+    attributes_df = attributes_df.set_index('Gene ID')
+
+    reference_df = reference_df.join(attributes_df, on='Gene ID')
+
+    return reference_df, results_df, ref_genome_seq
 
 
 def clustering_analysis(X, num_clusters):
@@ -127,40 +136,55 @@ def best_clusters_silhouette(X, out_path):
     fig = plt.figure(figsize=(10, 5), constrained_layout=True)
     ax = fig.gca()
     ax.plot(cluster_options, cluster_silhouette_scores)
-
-    [print(x, '\t', score) for x, score in zip(cluster_options, cluster_silhouette_scores)]
+    for i, j in zip(cluster_options, cluster_silhouette_scores):
+        print(i, '\t', j)
 
     plt.savefig(out_path)
     plt.close()
 
 
-def main(proj_dir):
+def run_gimme_motifs(target_fasta, output_dir, ref_genome_path):
+    command = "gimme motifs {input_file} {output_dir} --denovo -g {genome_ref}".format(input_file=target_fasta, output_dir=output_dir, genome_ref=ref_genome_path)
+    os.system(command)
+    return True
 
-    figure_dir = proj_dir / 'figures'
-    figure_dir.mkdir(parents=True, exist_ok=True)
 
-    diel_plotting = figure_dir / 'diel_plotting'
+def main(proj_dir, num_clusters):
+
+    results_dir = proj_dir / 'results'
+    results_dir.mkdir(parents=True, exist_ok=True)
+
+    diel_plotting = results_dir / 'diel_plotting'
     diel_plotting.mkdir(parents=True, exist_ok=True)
 
-    pca_plotting = figure_dir / 'pca_plotting'
+    pca_plotting = results_dir / 'pca_plotting'
     pca_plotting.mkdir(parents=True, exist_ok=True)
 
-    heat_map = figure_dir / 'heat_map'
+    heat_map = results_dir / 'heat_map'
     heat_map.mkdir(parents=True, exist_ok=True)
 
-    silhouette_dir = figure_dir / 'silhouette_plot'
+    silhouette_dir = results_dir / 'silhouette_plot'
     silhouette_dir.mkdir(parents=True, exist_ok=True)
 
-    dataframe_dir = figure_dir / 'cluster_references'
+    dataframe_dir = results_dir / 'cluster_references'
     dataframe_dir.mkdir(parents=True, exist_ok=True)
 
-    reference_df, results_df = load_data(proj_dir)
+    motif_fastas = results_dir / 'motif_fastas'
+    motif_fastas.mkdir(parents=True, exist_ok=True)
+
+    gimme_results = results_dir / 'gimme_motifs_results'
+    gimme_results.mkdir(parents=True, exist_ok=True)
+
+    reference_df, results_df, ref_genome_seq = load_data(proj_dir)
 
     X = results_df.to_numpy()
 
     best_clusters_silhouette(X, silhouette_dir / 'ideal_clustering.png')
 
-    for num_clusters in [9, 18, 36, 53]:
+    # [9, 18, 39, 59]
+
+    # for num_clusters in [39, 59]:
+    if num_clusters:
         
         clustering = clustering_analysis(X, num_clusters=num_clusters)
         labels = clustering.labels_
@@ -179,12 +203,65 @@ def main(proj_dir):
         plot_PCA_clusters(results_df, pca_plotting / 'PCA_plot_{}.png'.format(num_clusters))
         plot_heatmap(results_df, heat_map / 'heat_map_{}.png'.format(num_clusters))
         reference_df.to_csv(dataframe_dir / '{}_clusters_reference_df.tsv'.format(num_clusters), sep='\t')
+
+        for cluster in labels:
+            n_clust_dir = motif_fastas / '{:02}_clusters'.format(num_clusters)
+            n_clust_dir.mkdir(parents=True, exist_ok=True)
+
+            fasta_out = n_clust_dir / 'cluster_{:02}.fasta'.format(cluster)
+
+            cluster_df = reference_df.loc[cluster]
+
+            savePromoterFasta(ref_genome_seq, cluster_df, fasta_out)
+
+            n_cluster_motif_dir = gimme_results / '{:02}_clusters'.format(num_clusters) / 'cluster_{:02}'.format(cluster)
+            n_cluster_motif_dir.mkdir(parents=True, exist_ok=True)
+
+            run_gimme_motifs(fasta_out, n_cluster_motif_dir, proj_dir / 'NATL2A_genome_references' / 'onlyNATL2A.fna')
+            
     
 
-# # Parsons iMac path:
-# proj_dir = Path(r'/Users/konnorvonemster/Dropbox (MIT)/Lab Member Files/Current Lab Members/Konnor von Emster/2021_trained_diel_LFC_motif_search')
+def savePromoterFasta(ref_seq, gff_df, fasta_outpath):
+    yfr_seq_records = []
+    
+    for gene in [gff_df.iloc[x] for x in range(len(gff_df))]:
+        label = gene.ID
+        start = gene.start
+        end = gene.end
+        strand = gene.strand
+        product = gene['product']
 
-# macbook pro path:
-proj_dir = Path(r'/Users/kve/Dropbox (MIT)/Lab Member Files/Current Lab Members/Konnor von Emster/2021_trained_diel_LFC_motif_search')
+        cds_sequence = ref_seq[start-1:end]
+        if strand == '-':
+            cds_sequence = cds_sequence.reverse_complement()
 
-main(proj_dir)
+        # A good control I found was to translate the protein and observe if it made sense
+        protein_sequence = cds_sequence.translate(table=TRANSLATION_TABLE)
+
+        promoter_start = promoter_stop = 0
+
+        if strand == '+':
+            promoter_start = start-100
+            promoter_stop = start+50
+            promoter_region_sequence = ref_seq[promoter_start:promoter_stop]
+        elif strand == '-':
+            promoter_start = end-49
+            promoter_stop = end+101
+            promoter_region_sequence = ref_seq[promoter_start:promoter_stop]
+            promoter_region_sequence = promoter_region_sequence.reverse_complement()
+
+        # print(label, '\t', start, '\t', end, '\t', strand, '\t', cds_sequence[0:5] + '...' + cds_sequence[-5:])
+
+        gene_seq_record = SeqRecord(promoter_region_sequence, 
+                                    id=label, name=gene.name, 
+                                    description='promoter region of {} making {} on strand {}'.format(label, product, strand))
+        
+        # print(gene_seq_record)
+        yfr_seq_records.append(gene_seq_record)
+
+    SeqIO.write(yfr_seq_records, fasta_outpath, "fasta")
+
+proj_dir = Path(sys.argv[1])
+num_clusters = int(Path(sys.argv[2]))
+
+main(proj_dir, num_clusters)
