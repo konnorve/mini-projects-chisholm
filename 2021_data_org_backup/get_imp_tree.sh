1cb7114adeda2b808f0b175443924f5cca8af13c
Konnor von Emster <konnorvonemster@gmail.com>
2021-12-07 23:09:36 -0500

    adding projects

---
+++ 2021_data_org_backup/get_imp_tree.sh
@@ -0,0 +1,29 @@
+tree -v -d -L 3 \
+/nobackup1/a_coe \
+/nobackup1/giocapo \
+/nobackup1/jamiewb \
+/nobackup1/kve \
+/nobackup1/pmberube \
+/nobackup1/radzinsk \
+/nobackup1/sbiller \
+/nobackup1/shogle \
+/nobackup1/skearney \
+/nobackup1/thackl \
+/nobackup1/thomase2 \
+/pool001/a_coe \
+/pool001/giocapo \
+/pool001/jamiewb \
+/pool001/kve \
+/pool001/pmberube \
+/pool001/radzinsk \
+/pool001/sbiller \
+/pool001/shogle \
+/pool001/skearney \
+/pool001/thackl \
+/pool001/thomase2 \
+/nobackup1/chisholmlab \
+/nfs/chisholmlab001 \
+/nfs/chisholmlab002 \
+> /nfs/chisholmlab001/kve/2021_data_org_backup/all_dir_trees.txt
+
+sed "s/^/'/" /nfs/chisholmlab001/kve/2021_data_org_backup/all_dir_trees.txt > /nfs/chisholmlab001/kve/2021_data_org_backup/all_dir_trees_mod.txt
